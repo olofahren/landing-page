d021d19ac036497c909e90705b735b1e22693ed8
Olof Åhrén <olof.murgarden@gmail.com>
2026-03-17 09:19:01 +0100

    feat: add temp about content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,12 +2,16 @@
 	import FoldIn from '$lib/components/FoldIn.svelte';
 </script>
 
-<section class="hero w-full h-screen flex items-center justify-center text-white">
-	<div class="text-center px-8">
-		<h1 class="text-4xl md:text-8xl font-semibold tracking-tight leading-[1.05]">
-			<FoldIn>About</FoldIn>
-		</h1>
-		<p class="subtext mt-5 text-lg md:text-xl text-white/90">Minimal about page structure.</p>
+<section class="hero w-full h-screen flex items-center justify-center text-white flex flex-col">
+		<p class="subtext mt-5 text-lg md:text-xl text-white/90">Some text about me as a person might appear here in the future.</p>
+        <p class="subtext mt-5 text-lg md:text-xl text-white/90">In the meantime, take a look at this amazing photo</p>
+        <div class="mt-10 flex justify-center">
+            <FoldIn as="div" innerAs="div" className="flex flex-row gap-4">
+                <img
+                    src="src/lib/assets/profile.jpeg"
+                    class="h-auto w-64 rounded-lg object-cover"
+                />
+            </FoldIn>
 	</div>
 </section>
 
